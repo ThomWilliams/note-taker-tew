03ad77eb2f93289476694d7806edc3289c0a5f18
Thom Williams <thomwilliams1990@gmail.com>
2021-05-06 19:38:00 +0100

    JS: Server listener  working

---
+++ Develop/server.js
@@ -0,0 +1,19 @@
+
+// Requires express
+const express = require("express");
+const app = express();
+
+// Defines PORT
+const PORT = 5000;
+
+// Sets Up middleware and parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+// Sets Up Routes 
+app.use(express.static("public"));
+// app.use("/api", apiRoutes);
+// app.use("/", htmlRoutes)
+
+app.listen(PORT, () => 
+console.log (`App listening on PORT: ${PORT}`));
